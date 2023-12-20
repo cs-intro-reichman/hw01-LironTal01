d2851e285e748e205923d811d98dadc302608972
LironTal01 <154249302+LironTal01@users.noreply.github.com>
2023-12-20 21:19:04 +0200

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -5,7 +5,7 @@
  */ 
 public class Triangle {
 	public static void main(String[] args) {
-
+		
 	//Gets from the command line 3 integers as the lengths of the trinagle sides
 	int a = Integer.parseInt(args[0]);
 	int b = Integer.parseInt(args[1]);
@@ -13,10 +13,11 @@ public class Triangle {
 	
 	//Check if the legths of any two sides is greater than the length of the remaining side
 	if ((a + b > c) && (a + c > b) && (b + c > a)) {
-		System.out.println(a + ", " + b + ", " + c + ": True"); //Print the lengths + "True" if the three given integers form a triangle
+		System.out.println(a + ", " + b + ", " + c + ": true"); 
+	//Print the lengths + "True" if the three given integers form a triangle
 	}
 	else {
-		System.out.println(a + ", " + b + ", " + c + ": False");
+		System.out.println(a + ", " + b + ", " + c + ": false");
 	}
 }
 }
