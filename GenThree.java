c3bf4c7b484b23bb76acb442359d01464b537ced
LironTal01 <154249302+LironTal01@users.noreply.github.com>
2023-12-20 18:42:31 +0200

    Update GenThree.java

--- GenThree.java
+++ GenThree.java
@@ -8,7 +8,6 @@ public class GenThree {
 	//Gets 2 integers from command line as the range
 	int a = Integer.parseInt(args[0]);
 	int b = Integer.parseInt(args[1]);
-	
 	//Generates 3 random numbers
 	int num1 = (int)(Math.random()*(b-a+1)+a);
 	int num2 = (int)(Math.random()*(b-a+1)+a);
@@ -18,10 +17,9 @@ public class GenThree {
 	System.out.println(num1);
 	System.out.println(num2);
 	System.out.println(num3);
-
+		
 	//Finding the minimal number out of the 3 numbers
 	int min = Math.min(Math.min(num1,num2), num3);
-
 	System.out.println("The minimal generated number was " + min);
 	}
 }
